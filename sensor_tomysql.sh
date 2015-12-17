925f39847217976d7819e117780d734c550e287c
photonix <fiberlasers@gmail.com>
2015-12-18 00:12:37 +0200

    Switch to bash script folder to run python

--- sensor_tomysql.sh
+++ sensor_tomysql.sh
@@ -1,8 +1,14 @@
-cd /home/pi/sensor
+#!/bin/bash
+# Usage : sensor_tomysql.sh interval
+# interval is sampling period in seconds
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd $DIR
 
 while true
 do 
     python sensor_tomysql.py
-    sleep 10
+    sleep $1
 done
 
