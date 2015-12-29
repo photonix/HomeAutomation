66c7be09f62ccc7c2dc3d1f2d4f173688d3efcd5
photonix <fiberlasers@gmail.com>
2015-12-29 02:24:57 +0200

    Added daily and hourly plots. Bug fixed for summary_daily

---
+++ plotly_daily.py
@@ -0,0 +1,43 @@
+import sys
+import datetime
+import pymysql
+from plotly import tools
+import plotly.plotly as py
+import plotly.graph_objs as go
+import pandas as pd
+import lttb
+
+days = int(sys.argv[1])
+
+username = 'photonix'
+api_key = 'q99v5kud1t'
+py.sign_in(username, api_key)
+
+
+# Connect to the database
+connection = pymysql.connect(host='RPI0', user='user', password='user', db='home',
+                             charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+
+room_code = [1, 2, 3, 0]
+room_name = ['1', '2', '3', '4']
+temp = [None] * len(room_code)
+humid = [None] * len(room_code)
+
+fig = tools.make_subplots(rows=2, cols=1, shared_xaxes=True)
+
+for (code, name, i) in zip(room_code, room_name, range(len(room_code))):
+    SQL_query = "SELECT * FROM temp_humid_daily WHERE room={0} and (timestamp > DATE_SUB(NOW(), INTERVAL {1} DAY));".format(code, days)
+    df = pd.read_sql_query(SQL_query, connection)
+    df.index = df['timestamp']
+    temp[i] = go.Scatter(x=df.index, y=df['temp'], name='T_Room #'+name)
+    fig.append_trace(temp[i], 1, 1)
+    humid[i] = go.Scatter(x=df.index, y=df['humid'], name='H_Room #'+name, yaxis='y2' )
+    fig.append_trace(humid[i], 2, 1)
+
+fig['layout'].update(title='Home Temperature/Humidity - Daily')
+
+fig['layout']['yaxis1'].update(title='Temperature')
+fig['layout']['yaxis2'].update(title='Humidity')
+
+plot_url = py.plot(fig, filename='Home Monitoring Daily', auto_open=False)
+
--- summary_daily.py
+++ summary_daily.py
@@ -1,10 +1,17 @@
 
+# Usage: python summary_daily.py <days>
+
 import sys
 import datetime
 import pymysql
 import pandas as pd
 from sqlalchemy import create_engine
 
+if len(sys.argv) > 0:
+    days = int(sys.argv[1])
+else:
+    days = 3 #default value 3 days
+    
 engine = create_engine('mysql+pymysql://user:user@RPI0/home', echo=False)
 
 
@@ -12,19 +19,19 @@ room_code = [1, 2, 3, 0]
 room_name = ['1', '2', '3', '4']
 
 for (code, name, i) in zip(room_code, room_name, range(len(room_code))):
-    SQL_query = "SELECT * FROM temp_humid WHERE room={0};".format(code)
+    SQL_query = "SELECT * FROM temp_humid WHERE room={0} and (timestamp>=subdate(current_date, {1}) and timestamp<current_date);".format(code, days)
     df = pd.read_sql_query(SQL_query, engine)
     df.index = df['timestamp']
     df_resampled = df.resample('D', how='mean')
     df_resampled['room'] = code
-    df_resampled['timestamp'] = df_resampled.index
+    df_resampled['timestamp'] = df_resampled.index.date
     df_resampled = df_resampled[df_resampled['timestamp'] != datetime.date(datetime.datetime.now().year,
                                                                            datetime.datetime.now().month,
                                                                            datetime.datetime.now().day)]
 
-    SQL_query_daily = "SELECT * FROM temp_humid_daily WHERE room={0};".format(code)
+    SQL_query_daily = "SELECT * FROM temp_humid_daily WHERE room={0} and (timestamp>=subdate(current_date, {1}) and timestamp<current_date);".format(code, days)
     df_daily = pd.read_sql_query(SQL_query_daily, engine)
-
+    
     new_list = list(set(df_resampled['timestamp'].astype(str)) - set(df_daily['timestamp'].astype(str)))
     if len(new_list)>0:
         new_index = [pd.Timestamp(x) for x in new_list]
